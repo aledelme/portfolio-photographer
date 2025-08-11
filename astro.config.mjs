ec8f1d8f9da1f40b07e6e52362b7ddea6e995b38
aledelme <alejandrodelmedico@gmail.com>
2025-08-11 22:29:16 +0200

    Init commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://your-portfolio-site.com",
+  integrations: [],
+  image: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+  vite: {
+    css: {
+      transformer: "postcss",
+    },
+  },
+});
